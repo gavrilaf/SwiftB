865d42d1c7d799236000ca06ca8f25149270ec25
Eugen Fedchenko <eugenf78@gmail.com>
2020-08-05 09:15:29 +0300

    added Math module & Real type

---
+++ Sources/SwiftB/Stat/CentralMoment.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension SwiftB {
+    
+    public static func centralMoment<S: Sequence>(_ s: S, order: Int) -> S.Element where S.Element: FloatingPoint {
+        return 0
+        
+    }
+}
