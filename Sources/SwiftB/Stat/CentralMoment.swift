61468bba3fc0ee8eb6a8128022bafb9364a90254
Eugen Fedchenko <eugenf78@gmail.com>
2020-08-05 09:59:07 +0300

    added central moments calculation

--- Sources/SwiftB/Stat/CentralMoment.swift
+++ Sources/SwiftB/Stat/CentralMoment.swift
@@ -2,8 +2,22 @@ import Foundation
 
 extension SwiftB {
     
-    public static func centralMoment<S: Sequence>(_ s: S, order: Int) -> S.Element where S.Element: FloatingPoint {
-        return 0
+    public static func centralMoment<S: Sequence>(_ s: S, order: Int) -> S.Element where S.Element: Real {
+        typealias E = S.Element
         
+        let mean = SwiftB.mean(s)
+        
+        var count = 0
+        var sum: E = 0
+        for v in s {
+            sum += (v - mean).toPower(E(order))
+            count += 1
+        }
+        
+        if count == 0 {
+            return E(count)
+        }
+        
+        return sum / E(count)
     }
 }
---
+++ Tests/SwiftBTests/CentralMomentTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import SwiftB
+
+final class CentralMomentsTests: XCTestCase {
+    func testCentralMoment() {
+        let s = [3, -1, 1, 4.1, 4.1, 0.7]
+        let expected = [1, 0, 3.5847222222, -1.5999259259, 20.6794136574]
+        
+        for order in 0..<4 {
+            let moment = SwiftB.centralMoment(s, order: order)
+            let expected = expected[order]
+            
+            XCTAssertTrue(SwiftB.closeEqual(expected, moment, delta: 0.0000000001), "central moment for order \(order), expected \(expected), actual \(moment)")
+        }
+    }
+    
+    func testCentralMoment2() {
+        let s: [Float] = [1, 2, 3, 4, 5]
+        let expected: [Float] = [1, 0, 2, 0, 6.8]
+        
+        for order in 0..<4 {
+            let moment = SwiftB.centralMoment(s, order: order)
+            let expected = expected[order]
+            
+            XCTAssertTrue(SwiftB.closeEqual(expected, moment, delta: 0.01), "central moment for order \(order), expected \(expected), actual \(moment)")
+        }
+    }
+    
+    func testEmpty() {
+        let s: [Float] = []
+        let moment = SwiftB.centralMoment(s, order: 1) // Compiler crashes in case SwiftB.centralMoment([], order: 1)
+        XCTAssertTrue(SwiftB.closeEqual(0, moment, delta: 0.0000000001))
+    }
+    
+    func testOneItem() {
+        let moment = SwiftB.centralMoment([1.0], order: 1)
+        XCTAssertTrue(SwiftB.closeEqual(0, moment, delta: 0.0000000001))
+    }
+    
+}
